266be0cdbcbd2c878dff4b9f47818d309af6cf21
Hans Fernandez <hf08@my.fsu.edu>
2019-07-05 12:01:09 -0400

    Create iescrollbar.js

---
+++ iescrollbar.js
@@ -0,0 +1,13 @@
+if(navigator.appName.indexOf("Internet Explorer")!=-1 || navigator.userAgent.match(/Trident.*rv[ :]*11\./))    
+
+{
+
+$("#Form1").css('overflow', 'scroll');
+
+$("#divUnSelectedList").css('overflow', 'scroll');
+
+$("#divSelectedList").css('overflow', 'scroll');
+
+}
+
+ 
